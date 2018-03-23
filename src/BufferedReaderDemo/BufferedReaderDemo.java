fbaf4c38c19e73726c1d1fd6b241c7f3a0282d66
lizhijian <907257608@qq.com>
2018-03-23 09:04:08 +0800

    增加bufferedReader按行读方法

--- src/BufferedReaderDemo/BufferedReaderDemo.java
+++ src/BufferedReaderDemo/BufferedReaderDemo.java
@@ -9,16 +9,30 @@ import java.io.FileReader;
 public class BufferedReaderDemo {
     public static void main(String[] args) {
         FileReader fileReader = null;
+        BufferedReader bufferedReader = null;
         try{
             fileReader = new FileReader("buf.txt");
-            BufferedReader bufferedReader = new BufferedReader(fileReader);
-            int num = 0;
+            bufferedReader = new BufferedReader(fileReader);
+            /*int num = 0;
             char [] ch = new char[1024];
             while((num=bufferedReader.read(ch))!=-1){
                 System.out.println(new String(ch,0,num));
+            }*/
+            //按照行的方式进行读取
+            String line = null;
+            while((line=bufferedReader.readLine())!=null){
+                System.out.println(line);
             }
-        }catch (Exception e){
+           // String s1 = bufferedReader.readLine();
 
+        }catch (Exception e){
+          try{
+              if(bufferedReader!=null){
+                  bufferedReader.close();
+              }
+          }catch (Exception e1){
+              e1.printStackTrace();
+          }
         }
 
 
