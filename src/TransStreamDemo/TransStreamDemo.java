8f9183f3e3b878fea375f4eea304986d7a58d7bd
v_lizhijian <907257608@qq.com>
2018-03-23 15:56:34 +0800

    增加流转换

--- src/TransStreamDemo/TransStreamDemo.java
+++ src/TransStreamDemo/TransStreamDemo.java
@@ -1,23 +1,33 @@
 package TransStreamDemo;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 
 //转换流
 public class TransStreamDemo {
     public static void main(String[] args) throws Exception {
-       //获取键盘录入对象
+       //获取键盘录入对象(字节流)
         InputStream in = System.in;
         //将字节流对象转成字符流对象，使用转换流 InputStreamReader
         InputStreamReader isr = new InputStreamReader(in);
         //为了提高效率，将字符流进行缓冲技术高效操作，使用bufferedReader
         BufferedReader bufr = new BufferedReader(isr);
+
+
+        OutputStream out = System.out;
+        OutputStreamWriter osw = new OutputStreamWriter(out);
+
+        //键盘录入最常见写法
+        BufferedWriter bufw = new BufferedWriter(osw);
+
+
         String line = null;
         while((line=bufr.readLine())!=null){
             if("over".equals(line))
                 break;
-            System.out.println(line.toUpperCase());
+           // System.out.println(line.toUpperCase());
+            bufw.write(line.toUpperCase());
+            bufw.newLine();
+            bufw.flush();
         }
         bufr.close();
     }
---
+++ src/TransStreamDemo/TransStreamDemo2.java
@@ -0,0 +1,10 @@
+package TransStreamDemo;
+
+/**
+ * Created by Administrator on 2018/3/23.
+ */
+public class TransStreamDemo2 {
+    public static void main(String[] args) {
+        System.out.println("===========");
+    }
+}
